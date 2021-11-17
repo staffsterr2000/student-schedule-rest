300d4314c54b8ddc683ffce5ae74883c31575bad
Stanislav Roshchenko <rosenkostas@gmail.com>
2021-11-17 17:39:03 +0200

    Initial commit

---
+++ src/main/java/com/staffsterr2000/studentschedulerest/StudentScheduleRestApplication.java
@@ -0,0 +1,13 @@
+package com.staffsterr2000.studentschedulerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentScheduleRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentScheduleRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/staffsterr2000/studentschedulerest/StudentScheduleRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staffsterr2000.studentschedulerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentScheduleRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
